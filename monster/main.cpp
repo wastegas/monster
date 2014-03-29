3ff68fcf1677558e2039e77fd95442aa943c70df
wastegas <wastegas@gmail.com>
2014-03-29 11:19:36 -1000

    Initial Commit

---
+++ monster/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  monster
+//
+//  Created by Ronnie Baron on 3/29/14.
+//  Copyright (c) 2014 Ronnie Baron. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
