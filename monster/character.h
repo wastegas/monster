e94b55a1d0cd4bc4408129048b8d22c0e7f03068
wastegas <wastegas@gmail.com>
2014-03-29 11:20:34 -1000

    initial commit

---
+++ monster/character.h
@@ -0,0 +1,14 @@
+//
+//  character.h
+//  monster
+//
+//  Created by Ronnie Baron on 3/29/14.
+//  Copyright (c) 2014 Ronnie Baron. All rights reserved.
+//
+
+#ifndef __monster__character__
+#define __monster__character__
+
+#include <iostream>
+
+#endif /* defined(__monster__character__) */
