48eb51566224266ae7bd98dd856635099f2ad746
wastegas <wastegas@gmail.com>
2014-03-29 16:07:52 -1000

    added Hero class

---
+++ monster/hero.cpp
@@ -0,0 +1,9 @@
+//
+//  hero.cpp
+//  monster
+//
+//  Created by Ronnie Baron on 3/29/14.
+//  Copyright (c) 2014 Ronnie Baron. All rights reserved.
+//
+
+#include "hero.h"
---
+++ monster/hero.h
@@ -0,0 +1,20 @@
+//
+//  hero.h
+//  monster
+//
+//  Created by Ronnie Baron on 3/29/14.
+//  Copyright (c) 2014 Ronnie Baron. All rights reserved.
+//
+
+#ifndef __monster__hero__
+#define __monster__hero__
+
+#include "character.h"
+
+class Hero : public Character
+{
+private:
+public:
+};
+
+#endif /* defined(__monster__hero__) */
